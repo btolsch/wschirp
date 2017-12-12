88a09ab397cf77c2133d22c608e1c83861b6f621
Brandon Tolsch <btolsch@gmail.com>
2017-12-12 14:15:35 -0800

    Update main for lemonbar script

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,74 @@
 #include <sys/select.h>
-#include <sys/time.h>
+#include <unistd.h>
 
-#include <chrono>
 #include <iostream>
+#include <string>
 
+#include "i3ipc++/ipc-util.hpp"
 #include "i3ipc++/ipc.hpp"
 
-int main() {
+namespace {
+
+void printWorkspaceInfo(i3ipc::connection& conn) {
+  auto workspaces = conn.get_workspaces();
+  std::cout << "%{A5:i3-msg workspace next:}";
+  std::cout << "%{A4:i3-msg workspace prev:}";
+  for (const auto& ws : workspaces) {
+    std::cout << "%{A:i3-msg workspace " << ws->name << ":}%{+u}";
+    if (ws->urgent) {
+      std::cout << "%{U#bf700f}";
+    } else if (ws->focused) {
+      std::cout << "%{U#0f70bf}";
+    } else if (!ws->visible) {
+      std::cout << "%{U#888888}";
+    }
+    std::cout << " " << ws->name << " %{-u}%{A}";
+  }
+  std::cout << "%{U-}%{A}%{A}";
+}
+
+int main_() {
   i3ipc::connection conn;
 
-  conn.signal_workspace_event.connect([](const i3ipc::workspace_event_t &ev) {
-    std::cout << "workspace event" << std::endl;
-    if (ev.current) {
-      std::cout << ev.current->name << std::endl;
-    }
-  });
   conn.subscribe(i3ipc::ET_WORKSPACE);
   conn.connect_event_socket();
 
   int32_t event_fd = conn.get_event_socket_fd();
-  struct timeval timeout {
-    0, 500000
-  };
   fd_set readfds;
   FD_ZERO(&readfds);
+  FD_SET(STDIN_FILENO, &readfds);
   FD_SET(event_fd, &readfds);
-  auto t1 = std::chrono::steady_clock::now();
-  while (select(event_fd + 1, &readfds, nullptr, nullptr, &timeout) != -1) {
-    auto t2 = std::chrono::steady_clock::now();
-    std::cout << "elapsed: "
-              << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1)
-                     .count()
-              << "ms" << std::endl;
-    timeout.tv_sec = 0;
-    timeout.tv_usec = 500000;
-    if (!FD_ISSET(event_fd, &readfds)) {
-      std::cout << "timeout" << std::endl;
-      FD_SET(event_fd, &readfds);
-      t1 = std::chrono::steady_clock::now();
-      continue;
+  std::string stdin_line;
+  while (select(event_fd + 1, &readfds, nullptr, nullptr, nullptr) != -1) {
+    const bool stdin_set = FD_ISSET(STDIN_FILENO, &readfds);
+    const bool event_set = FD_ISSET(event_fd, &readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+    FD_SET(event_fd, &readfds);
+    if (stdin_set) {
+      std::getline(std::cin, stdin_line);
+      if (stdin_line.length() == 0) {
+        return 0;
+      }
     }
-    std::cout << "event" << std::endl;
-    conn.handle_event();
-    t1 = std::chrono::steady_clock::now();
+    if (event_set) {
+      conn.handle_event();
+    }
+    printWorkspaceInfo(conn);
+    std::cout << stdin_line << std::endl;
   }
 
   return 0;
+}
+
+}  // namespace
+
+int main() {
+  while (true) {
+    try {
+      return main_();
+    } catch (i3ipc::ipc_error&) {
+      // Wait for i3 to return if it's reloading.
+      sleep(1);
+    }
+  }
 }
